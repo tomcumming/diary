0d01f741635ff7e1ab290d96015947b993a02c81
Tom Cumming <mail@tomcumming.co.uk>
2020-04-03 19:42:25 +0100

    Pull out menu

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello" as string);
+function loaded() {
+  const nav = document.querySelector("nav");
+  const navToggle = document.querySelector("#toggle-nav");
+  navToggle.addEventListener("change", (e) => {
+    if ((e.target as HTMLInputElement).checked) {
+      nav.classList.add("open");
+    } else {
+      nav.classList.remove("open");
+    }
+  });
+}
+
+window.addEventListener("DOMContentLoaded", loaded);
