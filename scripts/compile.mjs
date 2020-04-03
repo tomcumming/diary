f9fd0a9f3934628626adee67a0ddbf4f8e9bdffd
Tom Cumming <mail@tomcumming.co.uk>
2020-04-03 18:28:50 +0100

    build single html file

---
+++ scripts/compile.mjs
@@ -0,0 +1,48 @@
+import * as fs from 'fs';
+import * as rollup from 'rollup';
+import rollupTypescript from 'rollup-plugin-typescript';
+
+import template from '../src/index.html.mjs';
+
+/** @param path {string} */
+async function includeFile(path) {
+    if(path.endsWith('.ts')) {
+        return includeTypescript(path);
+    } else {
+        return fs.readFileSync(path, 'utf8');
+    }
+}
+
+async function includeTypescript(path) {
+    /** @type {rollup.InputOptions} */
+    const inputOptions = {
+        input: path,
+        plugins: [
+            rollupTypescript()
+        ]
+    };
+
+    const bundle = await rollup.rollup(inputOptions);
+
+    /** @type {rollup.OutputOptions} */
+    const outputOptions = {
+        format: 'es',
+    };
+    const { output } = await bundle.generate(outputOptions);
+
+    if(output.length !== 1)
+        throw new Error(`Expected one chunk`);
+
+    const [ singleOutput ] = output;
+    return singleOutput.code;
+}
+
+async function main() {
+    fs.writeFileSync(
+        'dist/index.html',
+        await template(includeFile),
+        'utf8'
+    );
+}
+
+main();
---
+++ src/index.html.mjs
@@ -0,0 +1,17 @@
+export default async (includeFile) => `<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+${await includeFile('src/style.css')}
+        </style>
+        <script>
+${await includeFile('src/index.ts')}
+        </script>
+    </head>
+    <body>
+${await includeFile('src/body.html')}
+
+${await includeFile('src/templates.html')}
+    </body>
+</html>
+`;
