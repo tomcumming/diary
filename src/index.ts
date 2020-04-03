c978371ed5521621032143ec188a642ae5afc0db
Tom Cumming <mail@tomcumming.co.uk>
2020-04-03 19:55:14 +0100

    TS config

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 function loaded() {
   const nav = document.querySelector("nav");
   const navToggle = document.querySelector("#toggle-nav");
+
+  if (!nav || !navToggle) throw new Error("Can't find nav elements");
+
   navToggle.addEventListener("change", (e) => {
     if ((e.target as HTMLInputElement).checked) {
       nav.classList.add("open");
